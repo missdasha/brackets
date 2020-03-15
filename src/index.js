5347c10fd29572cc3a9b2de58092f79915f468ea
missukevitch <missukevitch@gmail.com>
2020-03-15 11:27:50 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    if(str.length % 2 !== 0) 
+        return false;
+    let config = [].concat(...bracketsConfig);
+    let stack = [];
+    for(let elem of str) {
+        if(elem === "7" || elem === "8" || elem === "|" ) {
+            if(elem === stack[stack.length-1])
+                stack.pop();
+            else stack.push(elem);
+        }
+        else {
+            let ind = config.indexOf(elem);
+            if(ind % 2 === 0)
+                stack.push(elem);
+            else {
+                if((config.indexOf(stack[stack.length-1]) + 1 ) === ind)
+                    stack.pop();
+                else return false;
+            }
+        }
+    }
+    return stack.length===0 ? true : false;
 }
